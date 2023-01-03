1830cd5c8884f4611014b962f21aa622b30f52f2
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-03 09:52:38 -0700

    title actualizado

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>foodZone</title>
+    <title>Saloon Cabo - <? echo $title?></title>
     <link rel="icon" href="assets/img/faveicon.png" type="image/gif" sizes="16x16">
     <!--  -->
     <link rel="stylesheet" href="assets/css/plugin.css">
